f970b42be5beba31f173c53180fafa4ec09f5939
namoo.jang <chrow16@naver.com>
2024-01-13 23:09:45 +0900

    chore: 불필요한 코드 제거

--- apps/main-app/src/main-app.module.ts
+++ apps/main-app/src/main-app.module.ts
@@ -3,7 +3,6 @@ import {
   Module,
   ValidationPipe,
 } from '@nestjs/common'
-import { MainAppService } from './main-app.service'
 import { HealthCheckModule } from '@slibs/health-check'
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
 import {
@@ -23,7 +22,6 @@ import * as controllers from './controller'
   ],
   controllers: [...ModuleUtils.exports(controllers)],
   providers: [
-    MainAppService,
     { provide: APP_INTERCEPTOR, useClass: RouterLoggerInterceptor },
     { provide: APP_FILTER, useClass: AppErrorFilter },
     { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
